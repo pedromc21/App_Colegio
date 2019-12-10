886e03b6ce500f567be12844117bf82b147f4651
Pedro Mar Can <58432970+pedromc21@users.noreply.github.com>
2019-12-10 12:13:20 -0600

    Col_01

---
+++ App_Colegio/Cole.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cole.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ App_Colegio/Cole.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Cole.UIClassic
+{
+    public class Class1
+    {
+    }
+}
