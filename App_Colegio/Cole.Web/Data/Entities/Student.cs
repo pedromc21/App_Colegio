00dff763b70330824961022db65208bb464aa350
Pedro Mar Can <58432970+pedromc21@users.noreply.github.com>
2019-12-11 12:44:36 -0600

    optional

--- App_Colegio/Cole.Web/Data/Entities/Student.cs
+++ App_Colegio/Cole.Web/Data/Entities/Student.cs
@@ -1,7 +1,30 @@
 ﻿namespace Cole.Web.Data.Entities
 {
+    using System.ComponentModel.DataAnnotations;
     public class Student
     {
+        public int Id { get; set; }
 
+        public int Persona_Id { get; set; }
+
+        [Display(Name = "Apellido Paterno")]
+        [MaxLength(50)]
+        public string Apellido_Paterno { get; set; }
+
+        [Display(Name = "Apellido Materno")]
+        [MaxLength(50)]
+        public string Apellido_Materno { get; set; }
+
+        [Display(Name = "Nombres")]
+        [MaxLength(50)]
+        public string Nombres { get; set; }
+
+        [Display(Name = "Matricula")]
+        [MaxLength(20)]
+        public string Matricula { get; set; }
+
+        [Display(Name = "Clave_Familia")]
+        [MaxLength(20)]
+        public string Clave_Familia { get; set; }
     }
 }
