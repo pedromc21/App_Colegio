e72de4f8591c54e69214deee42311d59a886b92a
Pedro Mar Can <58432970+pedromc21@users.noreply.github.com>
2019-12-12 15:27:15 -0600

    End Backend

---
+++ App_Colegio/Cole.Web/Controllers/API/StudentController.cs
@@ -0,0 +1,20 @@
+﻿namespace Cole.Web.Controllers.API
+{
+    using Cole.Web.Data;
+    using Microsoft.AspNetCore.Mvc;
+    [Route("api/[Controller]")]
+    public class StudentController : Controller
+    {
+        private readonly StudentRepository studentRepository;
+
+        public StudentController(StudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok(this.studentRepository.GetAll());
+        }
+    }
+}
